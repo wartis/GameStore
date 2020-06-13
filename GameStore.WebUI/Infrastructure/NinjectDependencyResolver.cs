35646b5fed1fc4162bc1ce7dfb23c13b74db0c34
wartis <kirill.klenichev@yandex.ru>
2020-06-13 21:10:06 +0300

    added db

--- GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Web.Mvc;
 using GameStore.Domain.Abstract;
+using GameStore.Domain.Concrete;
 using GameStore.Domain.Entities;
 using Moq;
 using Ninject;
@@ -31,16 +32,17 @@ namespace GameStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IGameRepository> mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
-            {
-                new Game() { Name = "GTA 5", Price=1499 }, 
-                new Game() { Name = "The last of us", Price=999}, 
-                new Game() { Name = "Civilisation 6", Price=2999}
-            });
+            //Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            //mock.Setup(m => m.Games).Returns(new List<Game>
+            //{
+            //    new Game() { Name = "GTA 5", Price=1499 }, 
+            //    new Game() { Name = "The last of us", Price=999}, 
+            //    new Game() { Name = "Civilisation 6", Price=2999}
+            //});
 
-            kernel.Bind<IGameRepository>().ToConstant(mock.Object); 
+            //kernel.Bind<IGameRepository>().ToConstant(mock.Object); 
             // Здесь размещаются привязки
+            kernel.Bind<IGameRepository>().To<EFGameRepository>(); 
         }
     }
 }
